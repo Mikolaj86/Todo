08bc308caa8c8a3ce80e07f35595d19bb7f0592c
Mikolaj86 <mikolaj.okon999@gmail.com>
2019-05-28 15:49:29 +0200

    added ability to add tasks

--- src/App.js
+++ src/App.js
@@ -15,12 +15,31 @@ class App extends Component {
             todo: ''
         }
     }
+
+    inputChangeHandler = event => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
+    addTask = event => {
+        event.preventDefault();
+        let newTask = {
+            task: this.state.todo,
+            id: Date.now(),
+            completed: false
+        };
+        this.setState({
+            todos: [...this.state.todos, newTask],
+            todo: ''
+        })
+    }
+
+
     render() {
         return (
             <div className="App">
                 <h1>To Do List</h1>
                 <List todos={this.state.todos}/>
-                <Form todos={this.state.todos}/>
+                <Form todos={this.state.todos} value={this.state.todo} inputChangeHandler={this.inputChangeHandler} addTask={this.addTask} />
             </div>
         );
     }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -4,8 +4,12 @@ import List from "./TodoList";
 const Form = props => {
     return (
         <form>
-            <input name="todo" type="text" placeholder="enter the task" />
-            <button>Add a task</button>
+            <input name="todo"
+                   type="text"
+                   placeholder="enter the task"
+                   value={props.value}
+                   onChange={props.inputChangeHandler} />
+            <button onClick={props.addTask}>Add a task</button>
             <button>Remove task</button>
         </form>
     )
