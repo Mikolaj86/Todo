bd361c826e6f5b35f3ae729f9748a61ae472f931
Mikolaj86 <mikolaj.okon999@gmail.com>
2019-07-08 10:13:04 +0200

    axios - remove all completed

--- src/App.js
+++ src/App.js
@@ -21,7 +21,6 @@ class App extends Component {
     }
 
 
-
        inputChangeHandler = event => {
            this.setState({[event.target.name]: event.target.value})
        }
@@ -67,28 +66,36 @@ class App extends Component {
                const {data} = await Axios.put(`http://127.0.0.1:8000/todoes_update/${id}/`, currentTodo)
            }
 
-           removeItems = (event) => {
+           removeItems = async (event) => {
                event.preventDefault();
-               let completed = [];
-               this.setState(prevState => {
-                   return {
-                       todos: prevState.todos.filter(todo => {
-                           if (todo.completed == true) {
-                               completed.push(todo)
-                           }
-                           return !todo.completed;
-                       })
-                   }
-               })
+
+               const completed = this.state.todos.filter(todo => {
+                   return todo.completed
+               });
+
                console.log(completed);
                const remove = completed.map( async (todo) => {
-                   console.log(todo);
+                   return todo
                    await Axios.delete(`http://localhost:8000/todoes_destroy/${todo.id}/`)
-
+                   console.log(todo);
                }
            )
-    }
 
+                const {data} = await Axios.get('http://127.0.0.1:8000/todoes_read/', {
+                 })
+                this.setState({todos : data})
+
+               // this.setState(prevState => {
+               //     return {
+               //         todos: prevState.todos.filter(todo => {
+               //             if (todo.completed == true) {
+               //                 completed.push(todo)
+               //             }
+               //             return !todo.completed;
+               //         })
+               //     }
+               // })
+    }
 
            updateFilter = st => {
                this.setState({filtertodos: st})
@@ -104,7 +111,6 @@ class App extends Component {
                    }
                    return todo
                });
-              // console.log("editing ", task)
                this.setState({
                    todos : todos,
                     todo: {
@@ -115,8 +121,6 @@ class App extends Component {
            }
 
            handleEditingDone = async (event, id) => {
-              // console.log('editing done')
-              // console.log(id)
                const currentTodo = {}
                if(event.keyCode === 13) {
                    const todos = this.state.todos.map(todo => {
@@ -128,15 +132,11 @@ class App extends Component {
                        }
                        return todo;
                    })
-                //   console.log(todos)
                    this.setState( {
                         todos
                    })
                    const {data} = await Axios.put(`http://127.0.0.1:8000/todoes_update/${id}/`, currentTodo)
                }
-
-
-
            }
 
            handleEditingChange = event => {
@@ -204,17 +204,16 @@ class App extends Component {
         } else if (this.state.filtertodos === 'complete') {
             todos = this.state.todos.filter(todo => todo.completed);
         }
-        // console.log(todos)
-        // console.log(this.state)
-        // debugger
+
         return (
             <div className="App">
                 <Header />
+                <Form todos={this.state.todos} value={this.state.todo} inputChangeHandler={this.inputChangeHandler}
+                      addTask={this.addTask} removeItems={this.removeItems} updateFilter={this.updateFilter} />
                 <List todos={todos} toggleComplete={this.toggleComplete} onDelete={this.onDelete}
                       handleEditing={this.handleEditing} handleEditingDone={this.handleEditingDone}
                       handleEditingChange={this.handleEditingChange} changedText={this.state.todo.changedText} />
-                <Form todos={this.state.todos} value={this.state.todo} inputChangeHandler={this.inputChangeHandler}
-                      addTask={this.addTask} removeItems={this.removeItems} updateFilter={this.updateFilter} />
+
             </div>
         );
     }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 const Todo = props => {
-     // console.log(props)
         return (
-            <div style={{display: "flex"}}>
+            <div style={{display: "flex"}} className="todo">
                 {props.todo.editing ? (
                     <input onKeyDown={event => props.handleEditingDone(event, props.todo.id)} onChange={event => props.handleEditingChange(event)}
                             type="text" value={props.changedText}/> )
                     :
-                    (<p key={props.todo.id}
+                    (<p style={{textDecoration: props.todo.completed ? 'line-through' : 'none'}} key={props.todo.id}
                        onClick={event => {
                            props.toggleComplete(props.todo.id)
                        }} >{props.todo.task}{props.todo.completed && ' Completed'}</p>)
