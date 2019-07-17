dfdb09d2598fb7c8e3b233736c3e241b23a1499f
Mikolaj86 <mikolaj.okon999@gmail.com>
2019-07-17 09:43:06 +0200

    add a little bit of styles

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Todo = props => {
         return (
-            <div style={{display: "flex"}} className="todo">
+            <div className="todo">
                 {props.todo.editing ? (
                     <input onKeyDown={event => props.handleEditingDone(event, props.todo.id)} onChange={event => props.handleEditingChange(event)}
                             type="text" value={props.changedText}/> )
@@ -12,11 +12,11 @@ const Todo = props => {
                            props.toggleComplete(props.todo.id)
                        }} >{props.todo.task}{props.todo.completed && ' Completed'}</p>)
                 }
-                <button onClick={event => props.handleEditing(props.todo.id)}>EDIT</button>
+                <div className="todo-buttons"><button onClick={event => props.handleEditing(props.todo.id)}>EDIT</button>
                 <button onClick={event => {
                     props.onDelete(props.todo.id)
                 }}>x
-                </button>
+                </button></div>
 
             </div>
         )
